1c719fb4b21014e8a2075cf340e9ad8bba415cac
PraveenRaj17 <65329340+PraveenRaj17@users.noreply.github.com>
2024-05-29 15:33:25 -0500

    Update livescore.py

--- livescore.py
+++ livescore.py
@@ -5,8 +5,8 @@ url = "https://livescore6.p.rapidapi.com/matches/v2/list-live"
 querystring = {"Category":"cricket"}
 
 headers = {
-	"X-RapidAPI-Key": "443409f4c6msh6286dfce62077e0p1ad446jsn97e9eb4f1f69",
-	"X-RapidAPI-Host": "livescore6.p.rapidapi.com"
+	"X-RapidAPI-Key": "xxx",
+	"X-RapidAPI-Host": "xxx"
 }
 
 response = requests.get(url, headers=headers, params=querystring)
